823f0b7569b59a556fa06ae25b0b6a7ab493c51e
Dihue <dihue21@gmail.com>
2023-04-01 00:01:04 -0300

    Primer aplicación

---
+++ Pastillas_Informaticas/Primer_Aplicacion/Primer_Aplicacion/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Primer_Aplicacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de una variable
+            int edad;
+
+            // Iniciación de una variable
+            edad = 23;
+
+            // Declaración e iniciación de una variable
+            int edadNueva = 30;
+
+            // Salida por consola
+            Console.WriteLine(edad);
+            Console.WriteLine(edadNueva);
+        }
+    }
+}
