40142a49a7e7a3e7a6bfb56bb7e3aef85bb4e0c7
Dihue <dihue21@gmail.com>
2023-04-01 00:03:10 -0300

    Sintaxtis básica - Parte 1

---
+++ Pastillas_Informaticas/Sintaxis_Basica/Sintaxis_Basica/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Sintaxis_Basica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de múltiples variables del mismo tipo
+            int edadPersona1, edadPersona2, edadPersona3, edadPersona4;
+
+            // Inicializa a todas las variables con el mismo valor
+            edadPersona1 = edadPersona2 = edadPersona3 = edadPersona4 = 21;
+
+            Console.WriteLine(edadPersona3);
+
+            // Declaración implícita de variable
+            // Asigna en tiempo de ejecución el tipo de la variable
+            // Pero luego ese valor asignado no puede ser modificado
+            var edadPersosna5 = 25;
+
+            Console.WriteLine(edadPersosna5);
+
+            double temperatura = 34.9;
+            int temperaturaMadrid;
+
+            // Conversión explícita o Casting
+            temperaturaMadrid = (int)temperatura;
+            // en este caso se da la perdida de información o precisión
+            // debido a que el valor double pierde su parte decimal
+            Console.WriteLine("Temperatura: " + temperaturaMadrid);
+
+            // Conversión implícita
+            // entre tipos compatibles pero de distinto alcance
+            int habitantesCiudad = 100000;
+            long habitantesCiudad2023 = habitantesCiudad;
+            Console.WriteLine(habitantesCiudad2023);
+
+            // Otro ejemplo
+            float pesoMaterial = 123.47F;
+            double pesoMaterialPreciso = pesoMaterial;
+            Console.WriteLine(pesoMaterialPreciso);
+
+            // Ver tabla de conversiones numéricas para más ejemplos
+
+            // Conversión de tipos (typeConverse)
+            // de String a numérico y viceversa
+
+        }
+    }
+}
