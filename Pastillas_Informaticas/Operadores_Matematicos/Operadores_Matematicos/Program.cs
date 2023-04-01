653310b41237ba17f457466f797b556a04b4c193
Dihue <dihue21@gmail.com>
2023-04-01 00:02:09 -0300

    Operadores aritmétricos

---
+++ Pastillas_Informaticas/Operadores_Matematicos/Operadores_Matematicos/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Operadores_Aritmeticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operaciones de manera directa
+
+            // Multiplicación entre enteros y decimales
+            Console.WriteLine(7 * 5);
+            Console.WriteLine(7 * 5.2);
+
+            // División entre enteros y decimales
+            Console.WriteLine(7.0 / 5.0);
+            Console.WriteLine(7.0 / 3);
+
+            // Resto de una división
+            Console.WriteLine(9 % 4);
+
+            int edad = 10;
+            // Usos del símbolo + como concatenador de Strings y Números
+            Console.WriteLine("Tienes una edad de " + edad++ + " años");
+
+            // Operador incremento como subfijo
+            edad++;
+
+            // Interpolación de Strings -> solo con C#
+            Console.WriteLine($"Tienes una edad de {--edad} años");
+        }
+    }
+}
