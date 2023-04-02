37fd6995a87538a631b08c6f1429ce54e27d2b15
Dihue <dihue21@gmail.com>
2023-04-01 21:38:30 -0300

    Uso de variables, asignaciones y operaciones

---
+++ Programacion_3/PrimerAplicacion/PrimerAplicacion/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PrimerAplicacion
+{
+    internal class Program
+    {
+        /*
+         * OBJETIVO: Comprender el uso de variables, asignaciones y
+         * operaciones de forma simple en C#
+         * 
+         * - Cree una aplicación de consola
+         * - Solicite el ingreso de dos valores por pantalla
+         * - Al final, muestre en forma descriptiva los resultados
+         * de aplicar las cuatro operaciones matemáticas básicas
+         */
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("\nIngresar el primer número");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nIngresar el segundo número");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" - - - - - - - - - - - - - - - ");
+            Console.WriteLine($"\nEl resultado de la suma es {A + B}");
+            Console.WriteLine($"\nEl resultado de la resta es {A - B}");
+            Console.WriteLine($"\nEl resultado de la multiplicación es {A * B}");
+            Console.WriteLine($"\nEl resultado de la división es {A / B}");
+        }
+    }
+}
