37d5680d561b75f176b501a18ad62205eb248ad6
Dihue <dihue21@gmail.com>
2023-04-02 20:55:47 -0300

    Sintaxis básica - Parte 2

---
+++ Pastillas_Informaticas/Sintaxis_Basica_2/Sintaxis_Basica_2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Sintaxis_Basica_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introducir el primer número: ");
+            // Conversión de string a int mediante la instrucción Parse
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introducir el segundo número: ");
+            // Conversión de string a int mediante la instrucción Parse
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"El resultado es {num1 + num2}");
+        }
+    }
+}
