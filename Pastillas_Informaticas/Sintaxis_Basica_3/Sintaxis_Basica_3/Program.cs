ebced733899c6f0eee31b4dfde73befc53cb7537
Dihue <dihue21@gmail.com>
2023-04-02 21:31:50 -0300

    Sintaxis básica - Variables y Constantes

---
+++ Pastillas_Informaticas/Sintaxis_Basica_3/Sintaxis_Basica_3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Sintaxis_Basica_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración e iniciación de una CONSTANTE
+            const int VALOR = 5;
+            const int VALOR2 = 7;
+
+            // Atajo mediante cw para el Console.WriteLine
+            Console.WriteLine("El valor de la constante es {0}", VALOR, VALOR2);
+            // {0} refleja el valor almacenado en el primer parámetro
+            Console.WriteLine("El valor de la constante es {1}", VALOR, VALOR2);
+            // {1} refleja el valor almacenado en el segundo parámetro
+
+
+            // Ejemplo: cálculo del área de un círculo
+            const double PI = Math.PI;
+
+            Console.WriteLine("\nIntroduce el radio de la circunferencia: ");
+
+            double radio = double.Parse(Console.ReadLine());
+
+            double area = radio * radio * PI;
+            double areaMath = Math.Pow(radio, 2) * PI;
+
+            Console.WriteLine($"El área del círculo es {area}");
+            Console.WriteLine($"El área del círculo es {areaMath}");
+        }
+    }
+}
