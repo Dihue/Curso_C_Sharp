be6f2a71d1c1314e0e627bdbaa1c0dfbfc453474
Dihue <dihue21@gmail.com>
2023-04-11 23:11:32 -0300

    Practicos sencillos

---
+++ Laboratorio_3/PracticosSencillos/PracticosSencillos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticosSencillos
+{
+    internal class Program
+    {
+        /*
+         * Realice un programa en c# que muestre los numeros del cero al
+         * 100 utilizando las siguientes estructuras while, do- while,for.
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
