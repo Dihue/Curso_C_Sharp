705fb8be599a88367efd9ac319c974b0d36e8953
Dihue <dihue21@gmail.com>
2023-04-14 20:24:02 -0300

    Métodos del tipo void

---
+++ Pastillas_Informaticas/Metodos_2/Metodos_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+ * Métodos en C#:
+ * Declaración y llamada a métodos void
+ * Paso de parámetros
+ */
+
+namespace Metodos_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Llamado al método
+            mensajeEnPantalla();
+
+            sumaNumeros(5,3);
+        }
+
+        // Método
+        static void mensajeEnPantalla()
+        {
+            Console.WriteLine("\nMensaje desde el método msjPantalla\n");
+        }
+
+        // Método con parámetros
+        static void sumaNumeros(int num1, int num2)
+        {
+            Console.WriteLine($"La suma de los números es {num1 + num2}");
+        }
+    }
+}
