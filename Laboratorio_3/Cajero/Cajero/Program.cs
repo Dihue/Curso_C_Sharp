8d136586751fc52d5fd4c2cafb1bf492eac26afb
Dihue <dihue21@gmail.com>
2023-04-18 18:53:42 -0300

    Ejerciocio Cajero - Base

---
+++ Laboratorio_3/Cajero/Cajero/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Desarrolle una aplicación en C#, para un CAJERO automático.
+ * La aplicación permitirá crear cuentas para JUBILADOS y PERSONAS en ACTIVIDAD.
+ * Los USUARIOS del cajero podrán depositar en su cuenta y realizar extracciones
+ * de la misma.
+ * Si el usuario es una persona en actividad laboral, podrá retirar hasta $20.000
+ * en concepto de adelanto de sueldo.
+ * Si el usuario es una persona jubilada podrá retirar en concepto de adelanto
+ * solo unos $10.000
+ * Cada operación de ingreso o extracción deberá registrar la fecha, el cajero y
+ * el monto de la operación.
+ * Los cajeros se identifican por su dirección y número de cajero.
+ * Si durante dos meses de operación, un usuario tuvo un saldo positivo superior
+ * a $20.000, se le ofrecerá un crédito pre acordado de $80.000 con lo cual, su
+ * nuevo límite de extracción en negativo será de $80.000
+ */
+
+namespace Cajero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Cajero
+    {
+
+    }
+
+    class Operacion
+    {
+
+    }
+
+    class CuentaBancaria
+    {
+
+    }
+
+    class Usuario
+    {
+
+    }
+}
