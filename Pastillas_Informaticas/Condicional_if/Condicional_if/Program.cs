fccc37fe43d6186456636b49180e8d5d4d6c9e04
Dihue <dihue21@gmail.com>
2023-04-25 19:58:01 -0300

    Condicional IF

---
+++ Pastillas_Informaticas/Condicional_if/Condicional_if/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+ * Declaración de variables booleanas
+ * Uso de operadores booleanos
+ * Conficional IF
+ */
+namespace Condicional_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de variable booleana
+            bool haceFrio = false;
+
+            Console.WriteLine("Original: " + haceFrio);
+
+            // Operador negación !
+            Console.WriteLine("Negado: " + !haceFrio);
+
+
+            int edad = 15;
+
+            Console.WriteLine("\nVamos a evaluar si eres mayor de edad");
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+        }
+    }
+}
