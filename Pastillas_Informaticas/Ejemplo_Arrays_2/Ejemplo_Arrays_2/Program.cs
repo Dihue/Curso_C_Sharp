c3983838e7d4470dd83558656da8f65cbaab28fd
Dihue <dihue21@gmail.com>
2023-05-03 21:42:09 -0300

    Revisión

---
+++ Pastillas_Informaticas/Ejemplo_Arrays_2/Ejemplo_Arrays_2/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Array implicitos
+ * Asignación
+ * Array anónimos
+ * Bucle FOR
+ */
+namespace Ejemplo_Arrays_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array implícito
+            // los datos pasados deben ser del mismo tipo
+            var datos = new[] { "Argentina", "Uruguay", "Brazil" };
+            var valores = new[] {12, 24, 56, 21.21, 35.35 }; // todo a double
+
+            // Array de objetos
+            Empleados[] arrayEmpleados = new Empleados[2];
+
+            // Asignación y creación del objeto
+            arrayEmpleados[0] = new Empleados("Virginia", 35);
+
+            Empleados Ana = new Empleados("Ana", 23);
+            arrayEmpleados[1] = Ana;
+
+            // Arrays de tipos o clases anónimas
+            var personas = new[]
+            {
+                new{ Nombre = "Juan", Edad = 45},
+                new { Nombre = "Maria", Edad = 35 },
+                new { Nombre = "Diana", Edad = 22 }
+            };
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.WriteLine(valores[i]);
+            }
+        }
+    }
+
+    class Empleados
+    {
+        public Empleados(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        string nombre;
+        int edad;
+    }
+}
