f80f2e66537c7bf63d116470980918b2a64c8e88
Dihue <dihue21@gmail.com>
2023-05-04 23:44:43 -0300

    Ejercicios del primer parcial

---
+++ Laboratorio_3/Parcial_1/Parcial_1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Parcial_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Llamada de la pregunta 1
+            Pregunta_1 pregunta_1 = new Pregunta_1();
+            Console.WriteLine("\n<- - - - - Resolución pregunta 1 - - - - ->");
+            pregunta_1.suma();
+
+            // Pregunta 3
+            Console.WriteLine("\n<- - - - - Resolución pregunta 3 - - - - ->");
+            Console.WriteLine("\n VECTOR");
+
+            int[] m2 = new int[2];
+            IMatriz mat2 = new MatrizLineal(m2);
+            mat2.mostrar();
+
+            Console.WriteLine("\n VECTOR multiplicado por 2");
+            Calcular c2 = new Calcular();
+            c2.multiplica_x2(mat2);
+
+            Console.WriteLine("\n MATRIZ");
+            int[,] m = new int[2, 2];
+            IMatriz mat = new MatrizCuadrada(m);
+            mat.mostrar();
+
+            Console.WriteLine("\n MATRIZ multiplicada por 2");
+            Calcular c = new Calcular();
+            c.multiplica_x2(mat);
+
+            Console.ReadLine();
+
+            // Pregunta 4
+            Console.WriteLine("\n<- - - - - Resolución pregunta 4 - - - - ->");
+            string nombre = "";
+            Persona persona = new Estudiante(nombre);
+            persona.Saludar();
+
+            // Pregunta 5
+            Console.WriteLine("\n<- - - - - Resolución pregunta 5 - - - - ->");
+            Estudiante estudiante = new Estudiante("Juan");
+            Console.WriteLine(estudiante.Nombre);
+        }
+    }
+}
