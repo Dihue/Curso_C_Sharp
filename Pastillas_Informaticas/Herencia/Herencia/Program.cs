f23a3993a8c8946188bd00d8274e11411cd4ebc3
Dihue <dihue21@gmail.com>
2023-05-07 14:47:13 -0300

    Uso del principio de sustitución

--- Pastillas_Informaticas/Herencia/Herencia/Program.cs
+++ Pastillas_Informaticas/Herencia/Herencia/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Herencia
+﻿/*
+ * Principio de Sustitución en la herencia : Diseño de Herencia
+ * (class ... es siempre un... class)
+ * Ayuda en la jerarquía de la Herencia
+ */
+namespace Herencia
 {
     internal class Program
     {
@@ -15,6 +20,26 @@
             humano.Pensar();
             Console.WriteLine("\nGorila: " + gorilla.NombreSerVivo);
             gorilla.Trepar();
+
+            Console.WriteLine("\n<- - - Principio de Sustitución - - ->\n");
+            // Principio de Sustitución
+            // Solo pueden acceder a los métodos declarados en la clase Mamífero
+            Mamifero animal = new Caballo("Ponyta");
+            Mamifero persona = new Humano("Juan");
+
+            // Uso en arrays
+            Mamifero[] almacenAnimales = new Mamifero[5];
+
+            almacenAnimales[0] = caballo;
+            almacenAnimales[1] = humano;
+            almacenAnimales[2] = gorilla;
+            almacenAnimales[3] = animal;
+            almacenAnimales[4] = persona;
+
+            // Solo se tiene acceso a los métodos de la clase definida (Mamiferos)
+            almacenAnimales[1].Respirar();
+            Console.WriteLine(almacenAnimales[3].NombreSerVivo);
+
         }
     }
 }
