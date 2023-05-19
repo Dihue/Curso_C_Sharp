96b6b4283e4429cff744ab9591d1a88b0b9a2f46
Dihue <dihue21@gmail.com>
2023-05-19 18:23:52 -0300

    Enunciado del ejercicio de herencia

---
+++ Pastillas_Informaticas/Herencia_Ejercicio/Herencia_Ejercicio/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Creación de tres clases:
+ *      Avión
+ *      Vehículo
+ *      Coche
+ * 
+ * Métodos comunes a las tres clases:
+ *      Arrancar motor
+ *      Parar motor
+ * 
+ * Método virtual:
+ *      Conducir
+ */
+
+namespace Herencia_Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
