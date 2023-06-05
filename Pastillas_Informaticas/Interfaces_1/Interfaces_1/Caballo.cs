21e7b2fb66cacb95fb92abba7201dc60653051df
Dihue <dihue21@gmail.com>
2023-06-05 19:14:31 -0300

    Uso de interfaces múltiples

--- Pastillas_Informaticas/Interfaces_1/Interfaces_1/Caballo.cs
+++ Pastillas_Informaticas/Interfaces_1/Interfaces_1/Caballo.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Interfaces_1
 {
-    internal class Caballo : Mamifero, IMamiferosTerrestres
+    internal class Caballo : Mamifero, IMamiferosTerrestres, IAnimalesYDeportes, ISaltoConPatas
     {
         // Constructor
         public Caballo(string nombreCaballo) : base(nombreCaballo)
         {
-
+            
         }
 
         // Método de clase
@@ -20,10 +20,21 @@ namespace Interfaces_1
             Console.WriteLine("Soy capaz de galopar");
         }
 
-        // Método que proviene de la interface
+        // Métodos que provienen de las interfaces
         public int NumeroDePatas()
         {
             return 4;
         }
+
+        public string TipoDeporte()
+        {
+            return "Carrera de caballos";
+        }
+
+        public bool EsOlimpico()
+        {
+            return false;
+        }
+
     }
 }
---
+++ Pastillas_Informaticas/Interfaces_1/Interfaces_1/ISaltoConPatas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_1
+{
+    internal interface ISaltoConPatas
+    {
+        // Métodos que solo son declarados, sin modificador de acceso
+        int NumeroDePatas();
+    }
+}
